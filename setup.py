0e036e6ca6a31ae69311638d3f93bcd8389f7cf6
brefra <frank_van_breugel@hotmail.com>
2020-03-10 21:53:46 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
-from setuptools import setup
-setup(
-  name='plugwise-plugwise',
-  packages=["plugwise", "plugwise.connections", "plugwise.messages", "plugwise.nodes"],
-  version='0.1',
-  license='MIT',
-  description='Async library for communicating with Plugwise devices by using a Plugwise stick',
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name='python-plugwise',
+  version="0.1",
   author='Frank van Breugel',
-  author_email='frank_van_breugel@hotmail.com',
-  url='https://github.com/brefra/python-plugwise',
-  download_url='https://github.com/brefra/python-plugwise/archive/0.1.tar.gz',
+  author_email='f.v.breugel@gmail.com',
+  description='Async library for Plugwise stick',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/brefra/python-plugwise",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Topic :: Home Automation",
+  ],
+  python_requires='>=3.6',
   install_requires=[
         'crcmod',
         'pyserial',
-  ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
   ],
 )
 
