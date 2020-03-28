4fe8bef35dfd73391eab4c1964cbb36b80379d5f
brefra <frank_van_breugel@hotmail.com>
2020-03-28 19:25:12 +0100

    Bump to 0.4
    Fixed install in readmin

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name='python-plugwise',
-  version="0.3",
+  version="0.4",
   author='Frank van Breugel',
   author_email='f.v.breugel@gmail.com',
   description='Async library for Plugwise stick',
