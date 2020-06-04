5a6b92fda8e5b1c670c01d3799a7151d9736f545
brefra <frank_van_breugel@hotmail.com>
2020-06-04 21:28:32 +0200

    bump version to 0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name='python-plugwise',
-  version="0.7",
+  version="0.8",
   author='Frank van Breugel',
   author_email='f.v.breugel@gmail.com',
   description='Async library for Plugwise stick',
