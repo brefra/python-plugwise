3ae7a49621bbf1368157d339b94217c84623c727
brefra <frank_van_breugel@hotmail.com>
2020-08-12 19:49:52 +0200

    Bump to v1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name='python-plugwise',
-  version="1.2.0",
+  version="1.2.1",
   author='Frank van Breugel',
   author_email='f.v.breugel@gmail.com',
   description='Async library for Plugwise USB-stick',
